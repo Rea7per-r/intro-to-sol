9379a50d62f24f89c201ec89d107ec811b8c6f44
Rea7per-r <118007761+Rea7per-r@users.noreply.github.com>
2023-03-02 15:24:29 +0530

    Update l2.sol

--- l2.sol
+++ l2.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
-contract converting{
-    function convertor() public payable returns(uint weival,uint ethval,uint gweival ){
+contract convertingg{
+    function convertorr() public payable returns(uint weival,uint ethval,uint gweival ){
         weival = msg.value;
         ethval = weival / 1 ether;
         gweival = weival / 1 gwei;
